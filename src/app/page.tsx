76e48af94687c02974183503bed22d269aacc8eb
Yekna <urosjeknic@gmail.com>
2024-02-02 15:18:34 +0100

    feat: add sleds to screen connected to the inputs type range with transitions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,21 @@ import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const ref = useRef<HTMLElement>(null);
+  const firstSliderRef = useRef<HTMLInputElement>(null);
+  const secondSliderRef = useRef<HTMLInputElement>(null);
+  const thirdSliderRef = useRef<HTMLInputElement>(null);
+  const fourthSliderRef = useRef<HTMLInputElement>(null);
+  const firstSledRef = useRef<HTMLImageElement>(null);
+  const secondSledRef = useRef<HTMLImageElement>(null);
+  const thirdSledRef = useRef<HTMLImageElement>(null);
+  const fourthSledRef = useRef<HTMLImageElement>(null);
 
   // The main container is responsive which means the distance between the snow pile container and the main container is dynamic so the distance should be made dynamic as well
   const [width, setContainerWidth] = useState(0);
+  const [sliderWidth, setSliderWidth] = useState(0);
+
+  // TODO: change size of slides based on main container width
+  // TODO: use useCallback to stop repeating code in onInput props
 
   useEffect(() => {
     const updateWidth = () => {
@@ -14,6 +26,10 @@ export default function Home() {
       if (ref.current) {
         const newWidth = ref.current.getBoundingClientRect().width;
         setContainerWidth(newWidth);
+      }
+      if (firstSliderRef.current) {
+        const newWidth = firstSliderRef.current.offsetWidth;
+        setSliderWidth(newWidth);
       }
     };
 
@@ -47,12 +63,26 @@ export default function Home() {
             style={{ marginBottom: width * 0.1 }}
           />
           <input
+            onInput={() => {
+              if (firstSledRef.current && firstSliderRef.current) {
+                firstSledRef.current.style.left = `${sliderWidth * (Number(firstSliderRef.current.value) / 12)}px`;
+              }
+            }}
+            ref={firstSliderRef}
             defaultValue={0}
             type="range"
             min="0"
             max="12"
             className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[85%]"
             // TODO: figure out a better way to set the width without using magic numbers
+          />
+          <Image
+            ref={firstSledRef}
+            src="/Szánkó_csúszkák.svg"
+            alt="Sled 1"
+            width={100}
+            height={100}
+            className="absolute left-0 z-10 pointer-events-none top-0 h-full"
           />
         </div>
         <div className="relative">
@@ -65,11 +95,25 @@ export default function Home() {
             style={{ marginBottom: width * 0.1 }}
           />
           <input
+            onInput={() => {
+              if (secondSledRef.current && secondSliderRef.current) {
+                secondSledRef.current.style.left = `${sliderWidth * (Number(secondSliderRef.current.value) / 12)}px`;
+              }
+            }}
+            ref={secondSliderRef}
             defaultValue={0}
             type="range"
             min="0"
             max="12"
             className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[85%]"
+          />
+          <Image
+            ref={secondSledRef}
+            src="/Szánkó_csúszkák.svg"
+            alt="Sled 1"
+            width={100}
+            height={100}
+            className="absolute left-0 z-10 pointer-events-none top-0 h-full"
           />
         </div>
         <div className="relative">
@@ -82,11 +126,25 @@ export default function Home() {
             style={{ marginBottom: width * 0.1 }}
           />
           <input
+            onInput={() => {
+              if (thirdSledRef.current && thirdSliderRef.current) {
+                thirdSledRef.current.style.left = `${sliderWidth * (Number(thirdSliderRef.current.value) / 12)}px`;
+              }
+            }}
+            ref={thirdSliderRef}
             defaultValue={0}
             type="range"
             min="0"
             max="12"
             className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[85%]"
+          />
+          <Image
+            ref={thirdSledRef}
+            src="/Szánkó_csúszkák.svg"
+            alt="Sled 1"
+            width={100}
+            height={100}
+            className="absolute left-0 z-10 pointer-events-none top-0 h-full"
           />
         </div>
         <div className="relative">
@@ -98,11 +156,25 @@ export default function Home() {
             height={0}
           />
           <input
+            onInput={() => {
+              if (fourthSledRef.current && fourthSliderRef.current) {
+                fourthSledRef.current.style.left = `${sliderWidth * (Number(fourthSliderRef.current.value) / 12)}px`;
+              }
+            }}
+            ref={fourthSliderRef}
             defaultValue={0}
             type="range"
             min="0"
             max="12"
             className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[85%]"
+          />
+          <Image
+            ref={fourthSledRef}
+            src="/Szánkó_csúszkák.svg"
+            alt="Sled 1"
+            width={100}
+            height={100}
+            className="absolute left-0 z-10 pointer-events-none top-0 h-full"
           />
         </div>
       </section>
